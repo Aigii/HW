52b30dd4bca63d3c3121394217189c2e4b5cc978
[Aigerim] <[t.aigerim19@mail.ru]>
2022-10-14 00:51:06 +0600

    exs47

--- hw C#47/Program.cs
+++ hw C#47/Program.cs
@@ -27,7 +27,7 @@ void PrintArray(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write(array[i, j] + "\t");
+            Console.Write(array[i, j]);
         }
         Console.WriteLine();
     }
